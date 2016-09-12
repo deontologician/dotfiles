7faa674f1f88b1a64141bd08ee6cdf315c7458f4
Josh Kuhn <deontologician@gmail.com>
2016-09-12 11:51:58 -0700

    Updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,3 +23,17 @@
 (require 'lang-rust)
 (require 'lang-haskell)
 (require 'josh-keyboard-maps)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (lua-mode zencoding-mode yaml-mode xterm-color xref-js2 workgroups2 web-mode unicode-fonts typescript-mode twittering-mode toml-mode shackle racer purescript-mode markdown-mode magit-gh-pulls json-mode js2-refactor ivy-hydra hungry-delete haskell-mode groovy-mode git-gutter-fringe gist frame-cmds flycheck-rust fish-mode elpy elm-mode counsel coffee-mode ace-window ace-jump-buffer))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
